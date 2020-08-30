bf5d5db2e429a5698e974709542eb5c7c81ad68c
chiukeung <461659545@qq.com>
2020-08-30 21:50:02 +0800

    Add files via upload

---
+++ Week_01/lc0024swap-nodes-in-pairs.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class Solution24 {
+	public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null) return head;
+		ListNode cur = head;
+		ListNode second = head.next;
+		
+		cur.next = swapPairs(second.next);
+		second.next = cur;
+		return second;
+
+    }
+
+}
